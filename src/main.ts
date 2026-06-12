a98c1acc38361fa0ab8055e3b2063ac22884bca1
Furqan Anwar <furqan@ezaccounts.ca>
2026-06-12 20:40:57 +0500

    fix: added cache-control header for allowed origins

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning', 'cache-control'],
   });
 
   app.use(cookieParser());
